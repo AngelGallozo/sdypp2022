ce086f3f1bfd33023c9527a031bf72c9e1a4d498
SebasUNLu <SebasUNLu@gmail.com>
2022-03-11 16:16:27 -0300

    Ejercicio 1
    Creo las clases Servidor, Cliente y sus respectivos JAR. Prueba exitosa en localhost

--- TP1/tp1_ejercicio1/src/main/java/sdypp_ejercicios/Cliente.java
+++ TP1/tp1_ejercicio1/src/main/java/sdypp_ejercicios/Cliente.java
@@ -1,10 +1,51 @@
 package sdypp_ejercicios;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Cliente 
 {
+    final int port = 9090;
+	final String ip = "localhost";
 
+	public Cliente(){
+        try{
+            Socket sc = new Socket(ip,port);
+            System.out.println("Conectado con el Servidor: "+port);
+            boolean flag=false;
+            Scanner scanner = new Scanner(System.in);
+
+            DataOutputStream salida= new DataOutputStream(sc.getOutputStream());//Canal de salida de datos al servidor
+            DataInputStream entrada= new DataInputStream(sc.getInputStream());//Canal de entrada de datos al servidor
+            while(!flag) 
+            {
+                String input = scanner.nextLine();
+
+                if(input.equals("exit")) {
+                	flag=true;
+                }
+
+                salida.writeUTF(input);//Le manda el mensaje al servidor mediante el canal de salida
+                String rec= entrada.readUTF();//se queda a la espera de algun mensaje del servidor
+                System.out.println("Respuesta del Servidor");
+                System.out.println(rec);
+            }
+
+            sc.close();
+            scanner.close();
+            System.out.println( "Servidor "+ port + " desconectado.");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello Cliente!" );
-    }    
+        Cliente cliente = new Cliente();
+    }
 }
--- TP1/tp1_ejercicio1/src/main/java/sdypp_ejercicios/Servidor.java
+++ TP1/tp1_ejercicio1/src/main/java/sdypp_ejercicios/Servidor.java
@@ -1,8 +1,57 @@
 package sdypp_ejercicios;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
 public class Servidor {
+
+    final int port = 9090;
+
+	public Servidor()
+    {
+        try{
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server escuchando en el puerto: "+port);
+
+            while (true){
+                Socket clientSocket = serverSocket.accept(); //Queda a la espera conexion del cliente
+                int port_cli= clientSocket.getPort();
+                System.out.println( "Atendiendo al cliente: " + port_cli);
+                atenderCliente(clientSocket);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void atenderCliente(Socket client)
+    {
+        try {
+            boolean flag= false;
+            DataOutputStream salida= new DataOutputStream(client.getOutputStream());    //Canal de salida de datos al cliente
+            DataInputStream entrada= new DataInputStream(client.getInputStream());      //Canal de entrada de datos al cliente
+            while ( !flag ) {
+                String rec = entrada.readUTF();//se queda a la espera de algun mensaje del cliente
+                if ( !rec.equals("exit") ) {
+                    System.out.println("Mensaje del cliente: " + rec);
+                    salida.writeUTF("El server responde: " + rec);//Le manda el mensaje al cliente mediante el canal de salida
+                } else {
+                    flag = true ;
+                    salida.writeUTF("El cliente se ha desconectado.");//Le manda el mensaje al cliente mediante el canal de salida
+                    salida.close(); //cierre canal salida
+                    client.close();//cierre conexion con el cliente
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello Servidor!" );
+        Servidor server = new Servidor();
     }    
 }
